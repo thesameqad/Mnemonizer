722ef0e41b8430419db71d7d4ee3db6835452bf9
thesameqad <thesameqad@gmail.com>
2012-06-08 01:46:05 +0300

    wcf fixes

---
+++ NumberConcatAlgorythm/DictionaryService/IDictionaryService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DictionaryService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IDictionaryService
+    {
+
+        [OperationContract]
+        string GetWordById(int id);
+
+        [OperationContract]
+        ulong GetIdForWord(string word);
+
+        [OperationContract]
+        int GetWordsCount();
+
+    }
+
+}
