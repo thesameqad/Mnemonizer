4d3c9beeecad25e74e3eeb2c50912af7bef67820
unknown <1@1-.(none)>
2012-06-14 22:11:14 +0300

    JSON service from Yana

---
+++ NumberConcatAlgorythm/ServiceJson/IServiceJson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace ServiceJson
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceJson" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceJson
+    {
+        [OperationContract]
+        /*[WebGet( ResponseFormat = WebMessageFormat.Json)]*/
+        [WebGet(UriTemplate = "?enterString={enterString}", ResponseFormat = WebMessageFormat.Json)]
+        string GetMnemonicString(string enterString);
+      [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        string GetOriginalString(string mnemonicString);
+        
+    }
+}
---
+++ NumberConcatAlgorythm/ServiceJson/ServiceJson.svc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Coder;
+using System.Numerics;
+
+namespace ServiceJson
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceJson" in code, svc and config file together.
+    public class ServiceJson : IServiceJson
+    {
+      private MnemonicCoder coder = new MnemonicCoder();
+      
+        public string GetMnemonicString(string enterString)
+        {
+
+            return coder.GetMnemonicString(enterString);
+            
+         
+        }
+
+       public string GetOriginalString(string mnemonicString)
+        {
+            return coder.GetOriginalString(mnemonicString);
+        }
+    }
+}
